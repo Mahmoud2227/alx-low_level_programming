2b83501f0e8d88e7579c7a8245fbf16453e08318
Mahmoud2227 <mahmoud.salah2227@yahoo.com>
2022-09-29 21:58:39 +0200

    fix code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,55 +1,51 @@
 #include "lists.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * insert_dnodeint_at_index - Add node at nth index
- *
- * @h: Head of node
- *
- * @idx: index
- *
- * @n: struct int
- *
- * Return: dlistint_t
+ *insert_dnodeint_at_index - inserts a new node at a given position
+ *@h:pointer to list
+ *@idx:index to insert at
+ *@n:integer data
+ *Return:address of new_node or NULL
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-  dlistint_t *new_node = malloc(sizeof(dlistint_t));
-  dlistint_t *current;
-  unsigned int count = 0;
+	unsigned int i = 0;
+	dlistint_t *current_node = *h;
+	dlistint_t *new_node = NULL;
 
-  if (h == NULL || new_node == NULL)
-  {
-    return (NULL);
-  }
-  new_node->n = n;
-  new_node->next = NULL;
-  new_node->prev = NULL;
-  current = *h;
-
-  if (idx == 0)
-  {
-    new_node = add_dnodeint(h, n);
-    return (new_node);
-  }
-  while (current)
-  {
-    if (current->next == NULL && count == idx - 1)
-    {
-      new_node = add_dnodeint_end(h, n);
-      return (new_node);
-    }
-    else if ((idx - 1) == count)
-    {
-      new_node->next = current->next;
-      new_node->prev = current;
-      current->next->prev = new_node;
-      current->next = new_node;
-      return (new_node);
-    }
-    count++;
-    current = current->next;
-  }
-  free(new_node);
-  return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	if (*h == NULL)
+	{
+		*h = new_node;
+		return (*h);
+	}
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	while (current_node != NULL)
+	{
+		if (i == idx)
+		{
+			new_node->next = current_node;
+			new_node->prev = current_node->prev;
+			current_node->prev->next = new_node;
+			current_node->prev = new_node;
+			return (new_node);
+		}
+		i++;
+		current_node = current_node->next;
+	}
+	/*end*/
+	if (i == idx)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	return (NULL);
 }
